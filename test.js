950f411cb7f1c0af5128ba13895cdc8f391515b5
mitchpeh <mitchmac@Mitchells-MacBook-Pro.local>
2017-04-05 13:58:54 -0400

    cut back to 1 script, fixed bracket error

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
-for (var i=0;i<length.myArray;i++)
+for (var i = 0; i < 7; i++)
 {
-  console.log(myArray[i]);
+
 }
 
-for (var i=0;i<7;i++)
-{
-  console.log(i);
-}
-}
+});
